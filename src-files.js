75591cf5f86f7ddfb00a69e59759e3474a2d226c
EricLBuehler <65165915+EricLBuehler@users.noreply.github.com>
2023-10-29 22:50:55 +0000

    Deploying to gh-pages from @ EricLBuehler/candle-lora@846f9eaa7a6316a21f23e003bbf636ee50099471 🚀

--- src-files.js
+++ src-files.js
@@ -1,6 +1,6 @@
 var srcIndex = JSON.parse('{\
 "candle_lora":["",[],["frozenconv.rs","frozenembed.rs","frozenlinear.rs","lib.rs","loraconv1d.rs","loraconv2d.rs","loraembed.rs","loralinear.rs"]],\
 "candle_lora_macro":["",[],["lib.rs"]],\
-"candle_lora_transformers":["",[],["bert.rs","blip.rs","blip_text.rs","dinov2.rs","falcon.rs","lib.rs","llama.rs","mistral.rs","mpt.rs","resnet.rs","stable_lm.rs","t5.rs","unsync_func.rs","varbuilder_utils.rs","with_tracing.rs"]]\
+"candle_lora_transformers":["",[],["bert.rs","bigcode.rs","blip.rs","blip_text.rs","dinov2.rs","falcon.rs","lib.rs","llama.rs","mistral.rs","mpt.rs","resnet.rs","stable_lm.rs","t5.rs","unsync_func.rs","varbuilder_utils.rs","with_tracing.rs"]]\
 }');
 createSrcSidebar();
