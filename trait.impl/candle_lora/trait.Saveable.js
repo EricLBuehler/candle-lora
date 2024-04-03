b8b23fab476d888c4b6ebe956118b2ca63ea33bb
EricLBuehler <65165915+EricLBuehler@users.noreply.github.com>
2024-04-03 11:33:10 +0000

    Deploying to gh-pages from @ EricLBuehler/candle-lora@84d7c427bb00f6cac33a3f01c5f9caf575774c91 🚀

---
+++ trait.impl/candle_lora/trait.Saveable.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {
+"candle_lora":[],
+"candle_lora_transformers":[["impl <a class=\"trait\" href=\"candle_lora/trait.Saveable.html\" title=\"trait candle_lora::Saveable\">Saveable</a> for <a class=\"struct\" href=\"candle_lora_transformers/llama/struct.LlamaLinear.html\" title=\"struct candle_lora_transformers::llama::LlamaLinear\">LlamaLinear</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- trait.impl/core/marker/trait.StructuralEq.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"candle_lora":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.76.0/core/marker/trait.StructuralEq.html\" title=\"trait core::marker::StructuralEq\">StructuralEq</a> for <a class=\"enum\" href=\"candle_lora/enum.MergeError.html\" title=\"enum candle_lora::MergeError\">MergeError</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
